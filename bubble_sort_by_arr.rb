74e69ff0bef36e3c1b9a1498a8c48974491f578f
addod19 <addodaniellarbi@gmail.com>
2019-09-05 11:54:22 +0000

    corrected errors(indentaions) with rubocop to make the code clean

--- bubble_sort_by_arr.rb
+++ bubble_sort_by_arr.rb
@@ -1,25 +1,24 @@
-def bubble_sort_by arr
+# frozen_string_literal: true
 
-    n = arr.length
+def bubble_sort_by(arr)
+  n = arr.length
 
-    if block_given?
-        loop do
-            swapped = false
+  if block_given?
+    loop do
+      swapped = false
 
-            (n-1).times do | i |
-                if yield(arr[i], arr[i+1]) > 0
-                    swapped = true
-                    arr[i], arr[i+1] = arr[i+1], arr[i]
-                end
-            end
-            break if not swapped
+      (n - 1).times do |i|
+        if yield(arr[i], arr[i + 1]).positive?
+          swapped = true
+          arr[i], arr[i + 1] = arr[i + 1], arr[i]
         end
+      end
+      break unless swapped
     end
-    arr
-
-
+  end
+  arr
 end
 
-greetings = ["hi", "hello", "hey"]
+greetings = %w[hi hello hey]
 
-print bubble_sort_by(greetings){ | left,right | left.length - right.length }
+print bubble_sort_by(greetings) { |left, right| left.length - right.length }
